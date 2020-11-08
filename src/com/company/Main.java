5d167de87a9c9eecb653aba3c3a5bac1085c16e7
David Ulloa <dulloa6310@gmail.com>
2020-11-08 11:08:08 -0500

    Something

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,22 +32,22 @@ public class Main extends Application {
     /* Tele-porter?, Add sword, Add minotaur */
 
     boolean[][] mazeOne = {
-            {false, false, false, false, true , true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, true , false, false, false, false, false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, true , true , true , false, true , true , true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, false, false, true , false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, true , false, false, false, false, false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true },
-            {false, true , false, true , true , true , false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true },
-            {false, true , false, false, false, true , false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {true , true , true , true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, true , false, false, false, true , false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, true , true , true , false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, false, false, false, false, true , true , true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {true , true , true , true , false, false, false, true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, false, false, false, false, true , false, true , false, false, false, false, false, false, false, true , true , true , true , false, false, false, false, false},
-            {false, true , true , true , true , true , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            {false, false ,false, false, false, false, false, true , false, false, false, false, false, false, false, true , false , true , true , false, true , true , true , true },
-            {false, true , true , true , true , true , true , true , false, false, false, false, false, false, false, true , false, false, false, false, false, false, false, false},
+            {false, false, false, false, true , true , false, false, false, false, false, false, false, false, true , false, false, false, false, false, false, false, false, false},
+            {false, true , false, false, false, false, false, true , false, true , true , true , true , false, true , false, true , true , true , true , false, true , true , false},
+            {false, true , true , true , false, true , true , true , false, true , false, false, false, false, true , false, false, false, false, false, false, false, false, false},
+            {false, false, false, true , false, true , false, false, false, true , false, true , false, false, false, false, true , true , true , false, true , false, false, false},
+            {false, true , false, false, false, false, false, true , false, true , false, true , true , true , true , false, false, false, true , false, true , false, true , true },
+            {false, true , false, true , true , true , false, true , false, false, false, false, false, false, true , false, true , false, true , false, false, false, false, true },
+            {false, true , false, false, false, false, false, true , true , false, true , true , true , false, true , false, true , false, true , false, true , true , false, false},
+            {true , true , true , true , false, true , false, false, false, false, false, false, false, false, false, false, true , false, false, false, false, false, false, false},
+            {false, true , false, false, false, true , false, true , true , true , false, true , true , false, true , true , true , false, true , true , true , true , true , false},
+            {false, true , true , true , false, true , false, false, false, false, false, true , false, false, false, false, false, false, false, false, false, false, true , false},
+            {false, false, false, false, false, true , true , true , false, true , false, true , false, true , false, true , true , true , true , true , true , false, true , false},
+            {true , true , true , true , false, false, false, true , false, true , false, true , false, true , false, false, false, false, false, false, false, false, true , false},
+            {false, false, false, false, false, true , false, true , false, false, false, true , false, true , true , false, true , true , true , false, true , false, true , false},
+            {false, true , true , true , true , true , false, false, false, true , false, true , false, false, false, false, false, false, false, false, true , false, false, false},
+            {false, false ,false, false, false, false, false, true , false, true , false, true , false, true , false, true , false, true , true , false, true , true , true , true },
+            {false, true , true , true , true , true , true , true , false, false, false, false, false, true , false, true , false, false, false, false, false, false, false, false},
     };
 
     boolean[][] mazeTwo = {
